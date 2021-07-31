705b6f17c888ac96ffad68c844db05dd110bb497
84MateuszStefanski <84mateuszstefanski@gmail.com>
2021-07-31 20:42:28 +0200

    działa

--- src/main/java/game/Hero.java
+++ src/main/java/game/Hero.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public class Hero {
 
-    private String name;
-    private String race;
+    private final String name;
+    private final String race;
     private int currentHealth;
-    private InventoryObject[] inventory = new InventoryObject[10];
+    private InventoryObject[] inventory;
     private boolean overloaded;
     private int inventoryWeigth;
 
@@ -15,7 +15,7 @@ public class Hero {
         this.name = name;
         this.race = race;
         this.currentHealth = 100;
-        this.inventory = inventory;
+        this.inventory = new InventoryObject[10];
         this.overloaded = false;
         this.inventoryWeigth = 0;
     }
@@ -32,44 +32,51 @@ public class Hero {
         return currentHealth;
     }
 
+    public int getInventoryWeigth() {
+        return inventoryWeigth;
+    }
+
     public InventoryObject[] getInventory() {
         return inventory;
     }
 
     public boolean isOverloaded() {
-        if (inventoryWeigth<=100){
+        if (inventoryWeigth <= 100) {
             overloaded = false;
-        }else {
+        } else {
             overloaded = true;
         }
 
         return overloaded;
     }
 
-    public void showInventory(){
-        for (InventoryObject object:inventory){
-            if(object!=null)
-            System.out.println(object);
+    public void showInventory() {
+        for (InventoryObject object : inventory) {
+            if (object != null)
+                System.out.println(object);
         }
     }
 
-    public boolean compareItems(InventoryObject inventoryObject){
-        try{for (int i = 0; i < inventory.length; i++) {
-           if (inventory[i].getName().equals(inventoryObject.getName())
-                    && inventory[i].getWeigth() == inventoryObject.getWeigth());
-           return true;
-        }}catch (NullPointerException e){
-          e.getCause();
-        }
-        return false;
-    }
+   // public boolean compareItems(InventoryObject inventoryObject) {
+//        try {
+//            for (int i = 0; i < inventory.length; i++) {
+//                    if (inventory[i].getName().equals(inventoryObject.getName())
+//                            && inventory[i].getWeigth() == inventoryObject.getWeigth()) ;
+//                    return true;
+//            }
+//        } catch (NullPointerException e) {
+//            return false;
+//        }
+//        return false;
+//    }
 
-    public boolean isThereFreePlaceInInventory(){
-        for (int i=0;i<inventory.length;i++){
-            if(inventory[i] == null){
+    public boolean isThereFreePlaceInInventory() {
+        for (int i = 0; i < inventory.length; i++) {
+            if (inventory[i] == null) {
                 return true;
             }
-        }return false;
+        }
+        return false;
     }
 
 
@@ -77,25 +84,44 @@ public class Hero {
 
         if (isOverloaded()) {
             System.out.println("Overload !!! To much inventory");
-        }else if (inventoryWeigth+inventoryObject.getWeigth()>=100){
+        }
+        if (inventoryWeigth + inventoryObject.getWeigth() > 100) {
             System.out.println("Cannot add item it will be to much weigth");
-            }else {
-            for (int i = 0; i < inventory.length; i++) {
-                if (isThereFreePlaceInInventory()) {
-                    if (compareItems(inventoryObject)) {
-                        inventory[i].setCount(inventory[i].getCount() + 1);
-                        inventoryWeigth += inventoryObject.getWeigth();
-                        break;
-                    } else {
-                        inventory[i] = inventoryObject;
-                        inventoryWeigth += inventoryObject.getWeigth();
-                        break;
-                    }
-                }else {
-                    System.out.println("Inventory full");
-                }
+        }
+
+        for (int i = 0; i < inventory.length; i++) {
+            if (inventory[i]!=null&&inventory[i].equals(inventoryObject)){
+                inventory[i].setCount(inventory[i].getCount() + 1);
+                inventoryWeigth += inventoryObject.getWeigth();
+                System.out.println(inventoryObject.getName() + " The same item added to inventory");
+                break;
+            }
+            else {
+                if (inventory[i]==null){
+                    inventory[i] = inventoryObject;
+                    inventoryWeigth += inventoryObject.getWeigth();
+                    System.out.println(inventoryObject.getName() + " Added to inventory");
+                    break;
+            }
             }
         }
+
+//        else if (compareItems(inventoryObject)) {
+//            for (int i = 0; i < inventory.length; i++) {
+//                inventory[i].setCount(inventory[i].getCount() + 1);
+//                inventoryWeigth += inventoryObject.getWeigth();
+//                System.out.println(inventoryObject.getName() + " The same item added to inventory");
+//                break;
+//            }
+//        } else if (compareItems(inventoryObject) == false && isThereFreePlaceInInventory()) {
+//            for (int i = 0; i < inventory.length; i++) {
+//                if (inventory[i]==null){
+//                inventory[i] = inventoryObject;
+//                inventoryWeigth += inventoryObject.getWeigth();
+//                System.out.println(inventoryObject.getName() + " Added to inventory");
+//                break;
+//            }}
+//        }
 
     }
 
--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -8,6 +8,18 @@ public class Main {
         InventoryObject inventoryObject2 = new InventoryObject("Bow",15.0);
         InventoryObject inventoryObject3 = new InventoryObject("Knife",1.0);
 
+        hero.addItem(inventoryObject1);
+        hero.addItem(inventoryObject1);
+        hero.addItem(inventoryObject3);
+
+        System.out.println(hero.getInventoryWeigth());
+
+        System.out.println(inventoryObject1.equals(inventoryObject2));
+
+        hero.addItem(inventoryObject2);
+        System.out.println(hero.getInventoryWeigth());
+
+        hero.showInventory();
 
 
 
